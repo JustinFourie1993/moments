ddb5f40ba6915d64bba30c214480406ca5110389
JustinFourie1993 <>
2023-11-17 10:52:41 +0000

    added ProfilPage.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -23,8 +24,7 @@ function App() {
             exact
             path="/"
             render={() => (
-              <PostsPage message="No results found. Adjust the search keyword." 
-              />
+              <PostsPage message="No results found. Adjust the search keyword." />
             )}
           />
           <Route
@@ -52,6 +52,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage/>} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
